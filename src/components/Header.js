3394abca1e240fc221e8484c66b99afc5bf5df6b
catherineyu8 <catherine.j.yu@gmail.com>
2025-03-12 14:48:51 -0400

    initial header and footer setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 const Header = () => {
-    return (
-      <header className="bg-gray-800 text-white p-4">
-        <h1 className="text-xl font-bold">My Website</h1>
-      </header>
-    );
-  };
-  
-  export default Header;
+	return (
+		<header className="flex justify-between items-center w-full pt-4 pb-4 border-b">
+			{/* left-aligned title */}
+			<a
+				className="nav-link text-xl font-bold flex justify-self-"
+				href="#"
+			>
+				Pottery @ Brown
+			</a>
+
+			{/* right-aligned nav bar */}
+			<nav className="flex justify-self-end gap-6">
+				<a className="nav-link" href="">
+					About Us
+				</a>
+				<a className="nav-link" href="">
+					Calendar
+				</a>
+				<a className="nav-link" href="">
+					Donate
+				</a>
+				<a className="nav-link" href="">
+					Join
+				</a>
+				<input type="text" placeholder="search" />
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
