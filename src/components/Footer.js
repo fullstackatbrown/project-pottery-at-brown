884e76ce97bc551ab3f50bf2ced9d847b99ebd92
catherineyu8 <catherine.j.yu@gmail.com>
2025-03-12 14:48:51 -0400

    initial header and footer setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,41 @@
 import Image from "next/image";
 
 const Footer = () => {
-    return (
-      <footer className="bg-gray-900 text-white p-4 text-center">
-        <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
-        <div className="flex grid grid-col-6 gap-4 p-10 justfy-center items-center">
-          <Image
-            className="row-span-2"
-            src="/brown.svg"
-            alt="Brown"
-            width={200}
-            height={200}
-          />
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+	return (
+		<footer className="flex flex-col gap-y-4 w-full">
+			<img
+				className="flex w-32"
+				src="/images/Brown-University-Logo.png"
+			></img>
+
+			<p>SAO Acknowledgement / Official Details go here</p>
+
+			{/* footer nav bar */}
+			<nav className="flex justify-between w-full pt-4 pb-4 border-t">
+				<a className="nav-link" href="">
+					pottery@brown.edu
+				</a>
+				<a className="nav-link" href="">
+					About Us
+				</a>
+				<a className="nav-link" href="">
+					Calendar
+				</a>
+				<a className="nav-link" href="">
+					Donate
+				</a>
+				<a className="nav-link" href="">
+					Join
+				</a>
+				<a className="nav-link" href="">
+					Mailing List
+				</a>
+				<a className="nav-link" href="">
+					Instagram
+				</a>
+			</nav>
+		</footer>
+	);
+};
+
+export default Footer;
