5ce0dbfee78f96b42168fa391e594483a9832262
henrygreenhut <henrysgreenhut@gmail.com>
2025-03-21 13:50:34 -0400

    updated routing so that header changes the page; added header + footer to all pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 import Image from "next/image";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<Header></Header>
 
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 				<p>yay main content!</p>
@@ -54,8 +51,6 @@ export default function Home() {
 					</a>
 				</div> */}
 			</main>
-
-			<Footer></Footer>
 		</div>
 	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,33 @@
+import Link from 'next/link';
+
 const Header = () => {
 	return (
 		<header className="flex justify-between items-center w-full pt-4 pb-4 border-b">
 			{/* left-aligned title */}
-			<a
-				className="nav-link text-xl font-bold flex justify-self-"
-				href="#"
-			>
+			<Link href="/" className="nav-link text-xl font-bold">
 				Pottery @ Brown
-			</a>
+			</Link>
 
 			{/* right-aligned nav bar */}
-			<nav className="flex justify-self-end gap-6">
-				<a className="nav-link" href="">
+			<nav className="flex gap-6">
+				<Link href="/about" className="nav-link">
 					About Us
-				</a>
-				<a className="nav-link" href="">
-					Calendar
-				</a>
-				<a className="nav-link" href="">
-					Donate
-				</a>
-				<a className="nav-link" href="">
+				</Link>
+				<Link href="/leadership" className="nav-link">
+					Leadership
+				</Link>
+				<Link href="/faq" className="nav-link">
+					FAQ
+				</Link>
+				<Link href="/workshops" className="nav-link">
+					Workshops and Schedule
+				</Link>
+				<Link href="/gallery" className="nav-link">
+					Gallery
+				</Link>
+				<Link href="/join" className="nav-link">
 					Join
-				</a>
+				</Link>
 				<input type="text" placeholder="search" />
 			</nav>
 		</header>
