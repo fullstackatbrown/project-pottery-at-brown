66b2b4261840c5a68f168ac4edf3f2a87368127f
catherineyu8 <catherine.j.yu@gmail.com>
2025-04-04 10:42:42 -0400

    initial gcal embed and workshops page

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e0c1e81aaa04a43b")
+/******/ 		__webpack_require__.h = () => ("db774f6294584208")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ src/app/workshops/GCalEmbed.js
@@ -0,0 +1,11 @@
+const GCalEmbed = () => {
+	return (
+		<iframe
+			src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&title&src=Y18xZjg2OWNhMThjN2YyNmQyYzc2NTJlYjQ2Njc2MmIyMDAyNTBlM2Y2YjViMzZkMmYzOTNjYzMzZDQ5NTlmMzU3QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00&color=%2398b178"
+			width="800"
+			height="600"
+		></iframe>
+	);
+};
+
+export default GCalEmbed;
