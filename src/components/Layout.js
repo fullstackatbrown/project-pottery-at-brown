914a64ad8dfecb03fa37fa8f8068430da40fc000
Aanya Agrawal <purplishwings@gmail.com>
2025-04-18 16:35:44 -0400

    fixed main page black bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1">{children}</main>
+      {children}
       <Footer />
     </div>
   );
