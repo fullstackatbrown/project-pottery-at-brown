384a799b08a02c985fe77d5c0896fd36a779c2c1
Evan Luo <headlessbear06@gmail.com>
2025-04-18 19:10:59 -0400

    starting backend

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 function Art(props){
 	return(
@@ -13,7 +14,9 @@ function Art(props){
 			/>
 			{/* Hover overlay */}
 			<div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-				<p className="text-white text-lg font-bold">{props.hoverText}</p>
+				<p className="text-white text-lg font-bold">
+					Photo Credit:{props.hoverText}
+					</p>
 			</div>
 		</div>
 		</>
@@ -21,7 +24,53 @@ function Art(props){
 }
 
 
+
+
 export default function Gallery() {
+
+	const [artGallery, setGallery] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		async function fetchProfiles() {
+		try {
+			const response = await fetch(
+			"https://api.cosmicjs.com/v3/buckets/potterybrown-production/objects?pretty=true&query=%7B%22type%22:%22leaderships%22%7D&limit=18&skip=0&read_key=d457O3yZQcIoACYAEF2Q6SYGoBQ5Q8waV36wRrfgaf9HOnjHVU&depth=1&props=slug,title,metadata,type,"
+			);
+			
+			if (!response.ok) {
+			throw new Error("Failed to fetch profiles");
+			}
+			
+			const data = await response.json();
+			
+			const admins = data.objects.filter(profile => profile.metadata.admin === true);
+			const studioAids = data.objects.filter(profile => profile.metadata.admin === false);
+			
+			setAdminProfiles(admins);
+			setStudioAidProfiles(studioAids);
+			setLoading(false);
+		} catch (err) {
+			setError(err.message);
+			setLoading(false);
+		}
+		}
+
+		fetchProfiles();
+	}, []);
+
+	const getImageUrl = (profile) => {
+		if (profile.metadata.image && profile.metadata.image.imgix_url) {
+		return profile.metadata.image.imgix_url;
+		}
+		else if (profile.metadata.image && profile.metadata.image.url) {
+		return profile.metadata.image.url;
+		}
+		return "/placeholder-image.png";
+	};
+
+
 	return (
 		<main className="bg-[url(/background.png)] bg-cover bg-center min-h-screen flex flex-col justify-center p-10 h-full">
 			<h1 className="text-[50px] font-hedvig text-[#513223] mx-auto justify-center items-center">
